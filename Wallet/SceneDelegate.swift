5a85d024e945d1929bc6de8bd37a6db3ef00bdc1
Maria Fedorova <mary08.01.99@yandex.ru>
2025-05-20 00:00:30 +0300

    создан проект

---
+++ Wallet/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Wallet
+//
+//  Created by Fedorova Maria on 19.05.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        let navController = UINavigationController(rootViewController: rootViewController)
+
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
